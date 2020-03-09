acc5e4a72272ae1591ca5e6189d0381b42ad0401
Danini Rossini <53859487+danielross95@users.noreply.github.com>
2020-03-09 09:51:26 +0100

    TextEditor Component

---
+++ TextEditor.vue
@@ -0,0 +1,31 @@
+<template>
+  <div @input="updateHTML" contenteditable="true" class="textEditor"></div>
+</template>
+<script>
+export default {
+  name: 'TextEditor',
+  props: {
+    value: undefined
+  },
+  data() {
+    return {
+      test: 'H<b>ELLO</b> <i>Wor</i>ld!'
+    }
+  },
+  mounted() {
+    this.$el.innerHTML = this.value
+  },
+  methods: {
+    updateHTML(e) {
+      this.$emit('input', e.target.innerHTML)
+    }
+  }
+}
+</script>
+<style scoped>
+.textEditor {
+  border: 1px solid lightblue;
+  min-height: 42px;
+  padding: 1rem 1.35rem;
+}
+</style>
